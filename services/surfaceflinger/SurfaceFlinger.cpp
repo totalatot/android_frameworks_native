a5d2821d4649e0d8549af74ae43fbc6877c09a45
totalatot <totalatot@gmail.com>
2024-05-11 17:44:50 +0800

    SurfaceFlinger: Don't cleanup resources from previous frame
    Causes bad lag on some legacy devices.
    
    Change-Id: I89913d214c7377c73bd307696dbf9aac2f9a5c0a
    
    SurfaceFlinger: Don't cleanup resources from previous frame on display
    
    we have this again ha ha
    
    Change-Id: I3ffe5247efdf5053a8ae5b9b43d3cdff1272a1fe
    
    SurfaceFlinger: Don't cleanup resources from previous frame on virtual display
    
    Causes bad lag on some legacy devices while WFD
    
    Change-Id: Iedb6aef8e416020d2b4be42b2f853955330c204a

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3251,12 +3251,16 @@ void SurfaceFlinger::onCompositionPresented(PhysicalDisplayId pacesetterId,
     }
 
     if (hasPacesetterDisplay && !pacesetterDisplay->isPoweredOn()) {
+#ifndef DISABLE_POSTRENDER_CLEANUP
         getRenderEngine().cleanupPostRender();
+#endif
         return;
     }
 
+#ifndef DISABLE_POSTRENDER_CLEANUP
     // Cleanup any outstanding resources due to rendering a prior frame.
     getRenderEngine().cleanupPostRender();
+#endif
 
     {
         std::lock_guard lock(mTexturePoolMutex);
@@ -3796,7 +3800,9 @@ void SurfaceFlinger::processDisplayChanged(const wp<IBinder>& displayToken,
 
     // Recreate the DisplayDevice if the surface or sequence ID changed.
     if (currentBinder != drawingBinder || currentState.sequenceId != drawingState.sequenceId) {
+#ifndef DISABLE_POSTRENDER_CLEANUP
         getRenderEngine().cleanFramebufferCache();
+#endif
 
         if (const auto display = getDisplayDeviceLocked(displayToken)) {
             display->disconnect();
